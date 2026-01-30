899dd3900df0f8292ce19ef47d169eee64c12b26
JohnnyHuang101 <123030055+JohnnyHuang101@users.noreply.github.com>
2026-01-29 20:02:25 -0800

    resolving web sockte user names

--- main.go
+++ main.go
@@ -3,29 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-
 type Client struct {
-    Connected bool
-    Name      string
+	Connected bool
+	Name      string
 }
-
-
-func randomName() string {
-    rand.Seed(time.Now().UnixNano())
-    return fmt.Sprintf(
-        "%s%s-%04d",
-        adjectives[rand.Intn(len(adjectives))],
-        animals[rand.Intn(len(animals))],
-        rand.Intn(10000),
-    )
-}
-
 
 // Define what a message looks like
 type Message struct {
@@ -35,31 +24,38 @@ type Message struct {
 	VideoTime float64 `json:"video_time"` // Current time in seconds (for seeking/sync)
 }
 
-func main() {
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all connections (change for production!)
+	},
+}
+var (
+	clients   = make(map[*websocket.Conn]*Client) // Key: Connection, Value: Connected Status
+	broadcast = make(chan Message)                // Broadcast channel
+	mutex     = sync.Mutex{}                      // Protects the clients map
+)
 
+var adjectives = []string{
+	"Bouncy", "Sneaky", "Cosmic", "Wiggly", "Fuzzy",
+	"Chaotic", "Sleepy", "Spicy", "Glorious", "Turbo",
+}
 
-	var upgrader = websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true // Allow all connections (change for production!)
-		},
-	}
-	var (
-		clients = make(map[*websocket.Conn]*Client) // Key: Connection, Value: Connected Status
-		broadcast = make(chan Message)             // Broadcast channel
-		mutex     = sync.Mutex{}                   // Protects the clients map
+var animals = []string{
+	"Panda", "Otter", "Raccoon", "Axolotl", "Ferret",
+	"Penguin", "Sloth", "Capybara", "Platypus", "Lemur",
+}
+
+func randomName() string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf(
+		"%s%s-%04d",
+		adjectives[rand.Intn(len(adjectives))],
+		animals[rand.Intn(len(animals))],
+		rand.Intn(10000),
 	)
-	
-	var adjectives = []string{
-		"Bouncy", "Sneaky", "Cosmic", "Wiggly", "Fuzzy",
-		"Chaotic", "Sleepy", "Spicy", "Glorious", "Turbo",
-	}
-	
-	var animals = []string{
-		"Panda", "Otter", "Raccoon", "Axolotl", "Ferret",
-		"Penguin", "Sloth", "Capybara", "Platypus", "Lemur",
-	}
+}
 
-
+func main() {
 
 	// 1. Serve the Frontend
 	// This tells Go to look for an "index.html" inside the "./public" folder
@@ -88,17 +84,20 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+	clientName := randomName()
+	newClient := &Client{Name: clientName}
+
 	// Register the new client
 	mutex.Lock()
-	clients[ws] = randomName()
+	clients[ws] = newClient
 	mutex.Unlock()
 	fmt.Println("New Client Connected")
 
-	conn.WriteJSON(Message{
+	ws.WriteJSON(Message{
 		Type:     "identity",
-		Username: client.Name,
+		Username: clientName,
 	})
-	
+
 	// Cleanup when function returns (user disconnects)
 	defer func() {
 		mutex.Lock()
